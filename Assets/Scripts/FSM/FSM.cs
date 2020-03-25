b18cd783624e4f10e456b96238534ce31e7d8386
narasu <narasu@users.noreply.github.com>
2020-03-25 14:36:48 +0100

    Added empty state machine, modified .gitignore to ignore Other folder

---
+++ Assets/Scripts/FSM/FSM.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM
+{
+    public GameObject owner { get; private set; }
+
+    private Dictionary<StateType, State> states;
+    private State currentState;
+
+    public void Initialize(GameObject owner)
+    {
+        this.owner = owner;
+    }
+
+    public void AddState(StateType newType, State newState)
+    {
+        states.Add(newType, newState);
+        states[newType].Initialize(this);
+    }
+
+    public void UpdateState()
+    {
+        currentState.Update();
+    }
+
+    public void GotoState(StateType key)
+    {
+        if(!states.ContainsKey(key))
+        {
+            return;
+        }
+
+        currentState.Exit();
+        currentState = states[key];
+        currentState.Enter();
+    }
+}
---
+++ Assets/Scripts/FSM/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateType { Idle, Chase, Return, Dead }
+public abstract class State
+{
+    protected FSM owner;
+
+    public void Initialize(FSM owner)
+    {
+        this.owner = owner;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
